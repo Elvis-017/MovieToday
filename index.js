9e3b0c2bec6c06384bf2f1809c6b75aa5315a89c
ELvis <proyectoselvis@gmail.com>
2022-06-25 18:18:54 -0400

    Improvements in API functions using template cloning

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const BASE_URL = "https://api.themoviedb.org/3/",
   MODAL_INFO = document.getElementById("modal-info"),
   MOVIE_IMAGE = document.getElementById("modal-image"),
   MOVIE_DESCRIPTION = document.getElementById("modal-description"),
-  UP_TEXT = document.getElementById("up-text");
+  UP_TEXT = document.getElementById("up-text"),
+  TEMPLATE_CARD = document.getElementById("template-card"),
+  TEMPLATE_CARD2 = document.getElementById("template-card2");
 
 let navlink = document.getElementsByClassName("nav-link"),
   cards = document.getElementsByClassName("card"),
@@ -62,10 +64,8 @@ function changeTheme(event) {
 
     MODAL.firstElementChild.firstElementChild.classList.add("bg-dark", "text-white")
 
-
     UP_TEXT.classList.add("text-light")
     UP_TEXT.classList.remove("text-dark")
-
 
     for (let index = 0; index < cards.length; index++) {
       const element = cards[index];
@@ -91,11 +91,9 @@ function changeTheme(event) {
     UP_TEXT.classList.remove("text-light")
     UP_TEXT.classList.add("text-dark")
 
-
     MODAL.firstElementChild.firstElementChild.classList.remove("bg-dark", "text-white")
     colorStateValue = 0
   }
-
 
   for (let index = 0; index < navlink.length; index++) {
     const element = navlink[index];
@@ -195,7 +193,6 @@ for (let index = 0; index < navlink.length; index++) {
     addLoader()
     setTimeout(chargeContent, 500);
   };
-
 }
 
 function showMovieDescription(e) {
@@ -213,62 +210,67 @@ function showMovieDescription(e) {
   MOVIE_DESCRIPTION.textContent = overview;
 }
 
+function clonnerTemplate(objectData, colClasses, cardClassValue, container) {
+  const templateNode = TEMPLATE_CARD.content.cloneNode(true)
+  let getElementDatasets = templateNode.querySelector(".card-info").dataset
 
+  getElementDatasets.title = objectData.title
+  getElementDatasets.image = objectData.poster_path
+  getElementDatasets.overview = objectData.overview
+  getElementDatasets.date = objectData.release_date
+
+  templateNode.querySelector(".col").classList.add(...colClasses)
+  templateNode.querySelector(".card").classList.add(cardClassValue)
+  templateNode.querySelector(".rating").textContent = objectData.vote_average
+  templateNode.querySelector(".card-title").textContent = objectData.title.length > 20 ? objectData.title.slice(0, 13) + "..." : objectData.title
+  templateNode.querySelector(".year").textContent = objectData.release_date.slice(0, 4)
+  templateNode.querySelector(".card-img-top").src = `https://image.tmdb.org/t/p/w500/${objectData.poster_path}`
+
+  container.appendChild(templateNode)
+}
 
 function upcomingData(endpoint, pageSize = null) {
   getMovies(endpoint, pageSize).then((response) => {
     for (key in response.results) {
       if (key == 6) break;
       const element = response.results[key];
-      UPCOMING_CONTAINER.insertAdjacentHTML(
-        "beforeend",
-        `<div class="col col-sm-6 portrait">
-          <div class="card ${colorStateValue == 0 ? "bg-light" : "bg-dark"}"  style="padding:0">
-               <div class="card-header">
-                   <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${
-                     element.poster_path
-                   }" alt="...">
-             </div>
-         </div>
-             </div>`
-      );
-
+      clonnerTemplate({
+          title: element.title,
+          poster_path: element.poster_path,
+          overview: element.overview,
+          release_date: element.release_date,
+          vote_average: element.vote_average,
+        },
+        ['col-sm-6'],
+        `${colorStateValue == 0 ? "bg-light" : "bg-dark"}`,
+        UPCOMING_CONTAINER
+      )
     }
-
   });
 }
 
 function endpointDataLink(endpoint, pageSize = null, searchParam = null) {
   getMovies(endpoint, pageSize, searchParam).then((response) => {
+
     if (response.results.length <= 0) {
       loadBtn.style.display = "none"
-      MOVIE_CONTAINER.insertAdjacentHTML(
-        "beforeend", `<div class="alet alert-danger px-3 py-3">No results</div>`)
+      MOVIE_CONTAINER.innerHTML = `<div class="alet alert-danger px-3 py-3">No results</div>`
 
     } else {
       for (key in response.results) {
         const element = response.results[key];
-        MOVIE_CONTAINER.insertAdjacentHTML(
-          "beforeend",
-          `<div class="col col-md-4 col-sm-4 col-xl-3 portrait">
-                 <div class="card ${colorStateValue == 0 ? "bg-light" : "bg-dark"}"  style="padding:0">
-                  <button onclick="showMovieDescription(this)"  data-image="${element.poster_path}"
-                  data-title="${element.title}"  data-overview="${element.overview}" data-date="${element.release_date}"
-                  class="card-info" data-toggle="modal" data-target="#exampleModal" >
-                 
-                  <span class="rating">  ${element.vote_average} <i class="fas fa-star"></i></span>
-                 
-                  <p class="card-title">${element.title.length > 20 ? element.title.slice(0,13) + "..." : element.title}</p>
-                    <p class="year">   ${element.release_date.slice(0, 4)}</p>
-                      </button>
-                       <div class="card-header">
-                           <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${
-                             element.poster_path
-                           }" alt="...">
-                     </div>
-                 </div>
-             </div>`
-        );
+
+        clonnerTemplate({
+            title: element.title,
+            poster_path: element.poster_path,
+            overview: element.overview,
+            release_date: element.release_date,
+            vote_average: element.vote_average,
+          },
+          ['col-md-4', 'col-sm-4', 'col-xl-3'],
+          `${colorStateValue == 0 ? "bg-light" : "bg-dark"}`,
+          MOVIE_CONTAINER
+        )
       }
     }
   });
@@ -285,6 +287,7 @@ function getMovies(endpoint, parPageSize, searchParam = null) {
       })
       .then((data) => {
         resolve(data);
+
       })
       .catch((error) => {
         reject(error);
